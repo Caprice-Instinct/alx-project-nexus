a69226db4c39bda7da28544c036722ffb88bdfad
Caprice-Instinct <linetwangui04@gmail.com>
2025-11-30 22:12:36 +0300

    Add filtering and search capabilities to JobViewSet

--- jobboard_backend/settings.py
+++ jobboard_backend/settings.py
@@ -57,6 +57,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ),
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
 }
 
 # SimpleJWT settings (optional, can be customized)
--- jobs/views.py
+++ jobs/views.py
@@ -1,5 +1,7 @@
-from rest_framework import viewsets
+
+from rest_framework import viewsets, filters
 from rest_framework.permissions import IsAuthenticated
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Job
 from .serializers import JobSerializer
 from users.permissions import IsAdmin, IsRegularUser
@@ -8,6 +10,10 @@ class JobViewSet(viewsets.ModelViewSet):
 	queryset = Job.objects.all()
 	serializer_class = JobSerializer
 	permission_classes = [IsAuthenticated]
+	filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+	filterset_fields = ['category', 'location', 'company']
+	search_fields = ['title', 'description', 'company', 'location']
+	ordering_fields = ['created_at', 'title', 'company', 'location']
 
 	def get_permissions(self):
 		if self.action in ['create', 'update', 'partial_update', 'destroy']:
